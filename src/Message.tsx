63b4e83c80ebae2a3490808786094a56b1292638
Ashraful Haque Sifat <ahsifat321@gmail.com>
2023-10-14 19:33:15 +0600

    1

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,3 +1,9 @@
 function Message(){
-    return <h1>Hello World</h1>;
+    const name = 'Sifat';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello Person</h1>;
+
 }
+
+export default Message;
